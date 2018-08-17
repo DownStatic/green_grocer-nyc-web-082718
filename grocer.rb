5ebaa165001721563d65fccac487e2a3934f01f3
Christopher Stull <chris.stull@gmail.com>
2018-08-17 17:34:21 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -29,8 +29,10 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  coupons.each do |inc,value|
-    if cart[]
+  coupons.each do |coupon|
+    if cart.keys.include?(coupon[:item])
+      if cart[coupon[:item][:count]] >= coupon[:num]
+        
 end
 
 def apply_clearance(cart)
