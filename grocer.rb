9ebf39b8fbbb12beee1c95a99a3b4cd899467144
Christopher Stull <chris.stull@gmail.com>
2018-08-17 17:26:15 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -11,11 +11,9 @@ def consolidate_cart(cart)
   
   counts = [0]*items.length
   cart.each do |hsh|
-    hsh.each do |item|
-      items.each_with_index do |it,index|
-        if item.keys[0] == it
-          counts[index] = counts[index] + 1
-        end
+    items.each_with_index do |it,index|
+      if hsh.keys[0] == it
+        counts[index] = counts[index] + 1
       end
     end
   end
